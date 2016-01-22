d7328229d96861ae40263f3f2b4073dea9ac2124
Derek <derekphan94@gmail.com>
2016-01-21 16:01:38 -0800

    Added stringPair

---
+++ src/main/java/edu/ucsb/hopefully_unhackable/mongodb/StringPair.java
@@ -0,0 +1,15 @@
+package edu.ucsb.hopefully_unhackable.mongodb;
+
+
+
+public class StringPair{
+	
+	private String file_id;
+	private String file_name;
+	
+	public StringPair(String file_id, String file_name){
+		this.file_id = file_id;
+		this.file_name = file_name;
+	}
+	
+}
